1a2f8f056becb218c9cf7c94fe091413d27dc4f3
Helen Agha <helenbelen111@gmail.com>
2018-11-12 18:42:17 -0500

    Modify for deployment

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import flask
 from flask import Flask, render_template, make_response, request
 import os
 import requests
-
 import json
 import re
 import datetime
@@ -11,13 +10,15 @@ import csv
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
+import oauth2client
+from oauth2client.client import OAuth2WebServerFlow
 import sys
 
 app = Flask(__name__)
 
 # This variable specifies the name of a file that contains the OAuth 2.0
 # information for this application, including its client_id and client_secret.
-CLIENT_SECRETS_FILE = "C:\\Users\\HelenBelen\\Documents\\Code\\python-da-app\\client_secret.json"
+CLIENT_SECRETS_FILE = "client_secret.json"
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.
@@ -29,7 +30,7 @@ API_VERSION = 'v3'
 # Note: A secret key is included in the sample so that it works.
 # If you use this code in your application, replace this with a truly secret
 # key. See http://flask.pocoo.org/docs/0.12/quickstart/#sessions.
-app.secret_key = 'xxxx'
+app.secret_key = os.urandom(24)
 
 
 @app.route("/")
@@ -86,8 +87,8 @@ def index():
 @app.route('/authorize')
 def authorize():
   # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES)
+  
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
 
   flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
 
@@ -111,8 +112,8 @@ def oauth2callback():
   # verified in the authorization server response.
   state = flask.session['state']
 
-  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
   flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
 
   # Use the authorization server's response to fetch the OAuth 2.0 tokens.
@@ -291,7 +292,8 @@ def write_csv(selected_rows, rows):
 def clear_credentials():
   if 'credentials' in flask.session:
     del flask.session['credentials']
-    del flask.session['selected_onpage']
+    if 'selected_onpage' in flask.session:
+      del flask.session['selected_onpage']
     app.logger.debug ('Credentials have been cleared.<br><br>')
 
   return flask.redirect(flask.url_for('index'))
@@ -310,8 +312,8 @@ if __name__ == "__main__":
 	# When running locally, disable OAuthlib's HTTPs verification.
   # ACTION ITEM for developers:
   #     When running in production *do not* leave this option enabled.
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
   # Specify a hostname and port that are set as a valid redirect URI
   # for your API project in the Google API Console.
-  app.run('localhost', 8080, debug=True)
+  app.run('localhost', 8080, debug=False)
